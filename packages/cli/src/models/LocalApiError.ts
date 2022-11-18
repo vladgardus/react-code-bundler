e252a60ecb5a83a95a530dcac42eeec8e88f2af8
Vlad <gardus.vlad@gmail.com>
2022-11-18 19:12:17 +0200

    added cli and local api

---
+++ packages/cli/src/models/LocalApiError.ts
@@ -0,0 +1,3 @@
+export interface LocalApiError {
+  code: string;
+}
---
+++ packages/local-api/dist/commands/serve.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.serveCommand = void 0;
+var commander_1 = require("commander");
+exports.serveCommand = new commander_1.Command()
+    .command('serve [filename]')
+    .description('Open a file for editing')
+    .option('-p, --port <number>', 'port to run server on', '4005')
+    .action(function (filename, options) {
+    if (filename === void 0) { filename = 'notebook.js'; }
+    console.log(filename, options);
+});
